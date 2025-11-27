30c527478354e722fb5402d8d081d91fcfccb57b
Tanny-6014 <tanmay.raykar@tatatechnologies.com>
2025-11-27 15:18:15 +0530

    Update WeatherForecastController.cs

--- Microservices/WBSService/Controllers/WeatherForecastController.cs
+++ Microservices/WBSService/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace WBSService.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast_Test")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
